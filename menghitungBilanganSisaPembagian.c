265bbfaeac3ccafaf531d915bf2a029cc454524c
I Dewa Putu Semadi <dewasemadi@apps.ipb.ac.id>
2021-02-03 08:08:00 +0800

    Update menghitungBilanganSisaPembagian.c

--- menghitungBilanganSisaPembagian.c
+++ menghitungBilanganSisaPembagian.c
@@ -4,9 +4,7 @@ int modulo(long long a, long long b)
 {
     long long hasil;
     if (a > 0 && b > 0)
-    {
         hasil = a % b;
-    }
     return hasil;
 }
 
@@ -18,9 +16,7 @@ int main()
     {
         scanf("%lld %lld %lld", &a, &b, &c);
         if (modulo(a, b) == c)
-        {
             counter++;
-        }
     }
     printf("%lld\n", counter);
     return 0;
