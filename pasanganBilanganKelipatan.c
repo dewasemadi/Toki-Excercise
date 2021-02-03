4281572ae785c0f8c457c96809ca95ed57df8c67
I Dewa Putu Semadi <dewasemadi@apps.ipb.ac.id>
2021-02-03 08:21:23 +0800

    Update pasanganBilanganKelipatan.c

--- pasanganBilanganKelipatan.c
+++ pasanganBilanganKelipatan.c
@@ -7,17 +7,13 @@ void kelipatan()
     // baca array
     scanf("%ld", &n);
     for (int i = 0; i < n; i++)
-    {
         scanf("%ld %ld", &a[i], &b[i]);
-    }
 
     // cari kelipatan
     for (int j = 0; j < n; j++)
     {
         if (a[j] % b[j] == 0 || b[j] % a[j] == 0)
-        {
             hasil += 1;
-        }
     }
     printf("%ld\n", hasil);
 }
