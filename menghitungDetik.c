6128370a01c16b1a3117e777e0a18a87504ca272
I Dewa Putu Semadi <dewasemadi@apps.ipb.ac.id>
2021-02-03 08:16:06 +0800

    Update menghitungDetik.c

--- menghitungDetik.c
+++ menghitungDetik.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
-int main(){
-int jam1, menit1, detik1;
-int jam2, menit2, detik2;
-int a,b, durasi;
-	scanf("%d %d %d", &jam1, &menit1, &detik1);
-	scanf("%d %d %d", &jam2, &menit2, &detik2);
 
-	a=(jam1*3600)+(menit1*60)+detik1;
-	b=(jam2*3600)+(menit2*60)+detik2;
-	
-	if(b<a){
-		b+=86400;
-	durasi=b-a;
-}
-	durasi=b-a;
-	printf("%d", durasi);
-return 0;
+int main()
+{
+    int jam1, menit1, detik1;
+    int jam2, menit2, detik2;
+    int a, b, durasi;
+
+    scanf("%d %d %d", &jam1, &menit1, &detik1);
+    scanf("%d %d %d", &jam2, &menit2, &detik2);
+
+    a = (jam1 * 3600) + (menit1 * 60) + detik1;
+    b = (jam2 * 3600) + (menit2 * 60) + detik2;
+
+    if (b < a)
+    {
+        b += 86400;
+        durasi = b - a;
+    }
+    durasi = b - a;
+    printf("%d", durasi);
+    return 0;
 }
